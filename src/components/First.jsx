3c088fac3c1afd80be41272ea3aec4704829bf7e
Aditya Yadav <aditya.yadav992636@gmail.com>
2025-10-11 17:16:31 +0530

    Upload on github

---
+++ src/components/First.jsx
@@ -0,0 +1,20 @@
+import Navbar from './Navbar'
+
+function First() {
+  return (
+     <>
+        <div className="main_image">
+            <Navbar />
+            <div className="profession">
+              <h2 className='im'>I'm</h2>
+              <h1 className='work'>Software Enginnering</h1>
+            </div>
+           <div className="text_container">
+            <h1 className="behind-text">ADITYA YADAV</h1>
+           </div>
+        </div>
+    </>
+  )
+}
+
+export default First
